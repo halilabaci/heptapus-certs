4249a515d5c2d52442bb887e8bcfadb0d93e1503
Samet Unsal <sametutku16@gmail.com>
2026-04-09 16:33:21 +0300

    fix: close missing div in discover page post card

--- heptacert/frontend/src/app/discover/page.tsx
+++ heptacert/frontend/src/app/discover/page.tsx
@@ -473,6 +473,7 @@ export default function DiscoveryPage() {
                     </div>
                   </div>
                 </div>
+              </div>
 
               {/* CTA Button */}
               {viewer && (
